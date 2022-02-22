35d25a3c17dbfb945be4000ff9da505d2f569cb3
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-02-21 23:17:29 -0300

    Article Component

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./styles.scss";
 
-const Article = () => {
-  return <div>Article</div>;
+export type ArticleType = {
+  imgUrl: string;
+  date: string;
+  text: string;
 };
+
+const Article = ({ imgUrl, date, text }: ArticleType) => (
+  <div className="gpt3__blog-container_article">
+    <div className="gpt3__blog-container_article-image">
+      <img src={imgUrl} alt="blog_image" />
+    </div>
+    <div className="gpt3__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Read Full Article</p>
+    </div>
+  </div>
+);
 
 export default Article;
