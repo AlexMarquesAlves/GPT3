06011932f8cd8c5302851c923240ff74aabfa7f5
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-02-21 23:06:29 -0300

    Feature Component

--- src/components/Feature/index.tsx
+++ src/components/Feature/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./styles.scss";
 
-const Feature = () => {
-  return <div>Feature</div>;
+export type FeatureType = {
+  title: string;
+  text: string;
 };
+
+const Feature = ({ title, text }: FeatureType) => (
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
 export default Feature;
