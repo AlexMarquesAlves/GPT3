0921203d88a9ae38c2c3439bf041e80a46f1e5d1
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-02-21 23:00:32 -0300

    Brands Section

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 import "./styles.scss";
 
-const Brand = () => {
-  return <div>Brand</div>;
-};
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt="google logo" />
+    </div>
+    <div>
+      <img src={slack} alt="slack logo" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassian logo" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox logo" />
+    </div>
+    <div>
+      <img src={shopify} alt=" shopify logo" />
+    </div>
+  </div>
+);
 
 export default Brand;
